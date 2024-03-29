d094c3217858a56bd86a85bde3fc8dd6773c2e78
Artem <atremat@gmail.com>
2024-03-30 01:20:55 +0200

    default contacts render

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,17 @@
-export default function Contact() {
-  return <></>;
+import { FaPhone } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+
+export default function Contact({ name, number }) {
+  return (
+    <li>
+      <div>
+        <FaUser />
+        <p>{name}</p>
+      </div>
+      <div>
+        <FaPhone />
+        <p>{number}</p>
+      </div>
+    </li>
+  );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,11 @@
-export default function ContactList() {
-  return <></>;
+import Contact from "../Contact/Contact";
+
+export default function ContactList({ contacts }) {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return <Contact key={id} name={name} number={number} />;
+      })}
+    </ul>
+  );
 }
