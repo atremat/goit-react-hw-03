8d9876729c221de0c510fc0ce729f93596cbe031
Artem <atremat@gmail.com>
2024-03-30 00:35:23 +0200

    files created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "modern-normalize";
 import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
